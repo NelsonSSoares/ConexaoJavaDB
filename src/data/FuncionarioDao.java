3e303d9af0a47cad15e764acb3a744cbb5ec196f
Nelson de Sousa <punkpunkada@gmail.com>
2020-11-16 14:06:23 -0200

    Aula 11 Parte 4 Concluida, usando metodo PreparedStatement para armazenar informações no DB

--- src/data/FuncionarioDao.java
+++ src/data/FuncionarioDao.java
@@ -6,7 +6,8 @@ package data;
 import java.sql.Connection; // responsavel pela conexão com o banco
 import java.sql.DriverManager; // Gerenciamento do endereço e log sql
 import java.sql.SQLException; // tratamento de erros sql
-import java.sql.Statement; // sessão pata executar operação no banco (canal de acesso) executa insert updates e delete
+import java.sql.PreparedStatement; // biblioteca que gera sessão(autorização) e conexão com DB de modo mais compriendivel 
+//import java.sql.Statement; // sessão pata executar operação no banco (canal de acesso) executa insert updates e delete
 
 
 
@@ -15,7 +16,7 @@ import java.sql.Statement; // sessão pata executar operação no banco (canal d
 public class FuncionarioDao {
     
     Connection con; // variavel responsavel pela conexão
-    Statement st; // variavel que executa os comandos sql, por meio de sessão de autorização
+    PreparedStatement st; // variavel que executa os comandos sql, por meio de sessão de autorização
     
     public boolean conectar(){
         
@@ -28,8 +29,7 @@ public class FuncionarioDao {
         try {
             Class.forName("com.mysql.jdbc.Driver");//  indica o endereço do driver
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/controle","root","");// responsavel pela indicação do endereço do banco de dados e loggin no banco
-            st = con.createStatement();// cria uma sessão com o banco, autorização para execução de comandos sql
-            
+           // st = con.createStatement();// cria uma sessão com o banco, autorização para execução de comandos sql            
             return true; 
         } catch (ClassNotFoundException | SQLException ex) { //unificando o tratatamento de erro em catch só
         
@@ -47,9 +47,14 @@ public class FuncionarioDao {
         
         try {
         
-        String sql; // variavel String que vai receber o comando sql
-        sql = "INSERT INTO tb_funcionario VALUES('"+ func.getMatricula() +"','"+ func.getNome() +"','"+ func.getCargo() +"',"+ func.getSalario() + ")";
-        status = st.executeUpdate(sql); // executa o comando sql atraves da sessão iniciada no banco de dados e armazena na variavel status
+        //String sql; // variavel String que vai receber o comando sql
+        //sql = "INSERT INTO tb_funcionario VALUES('"+ func.getMatricula() +"','"+ func.getNome() +"','"+ func.getCargo() +"',"+ func.getSalario() + ")";
+        st = con.prepareStatement("INSERT INTO tb_funcionario VALUES(?,?,?,?)"); // metodo preoperadeStatment para inserção de dados no banco de dados de forma mais legivel, porem dados tem que ser inseridos conforme coluna do banco de dados 
+        st.setString(1, func.getMatricula()); // Numero indica a ordem de inserção das colunas
+        st.setString(2, func.getNome()); // coloca dados no banco que estão encapsulados na classe funcionario
+        st.setString(3, func.getCargo());
+        st.setDouble(4, func.getSalario()); // como salario é valor real tem que colocar double ao invez de strings
+        status = st.executeUpdate(); // executa o comando sql atraves da sessão iniciada no banco de dados e armazena na variavel status
         return status; // Retorna o codigo da operação quando concluida
         } catch (SQLException ex) {
             System.out.println(ex.getErrorCode()); // revela o codigo do erro na IDE
